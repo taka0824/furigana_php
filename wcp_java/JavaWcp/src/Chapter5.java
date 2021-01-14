fb754a4833f61956832f86d7f016e860a1c90c01
takanari <taka0824nari@gmail.com>
2021-01-14 13:40:07 +0900

    [finish]chapter5

---
+++ wcp_java/JavaWcp/src/Chapter5.java
@@ -0,0 +1,23 @@
+
+public class Chapter5 {
+	public static void main(String[] args) {
+		long number = 56L;
+		System.out.println(number);
+		double number2 = 23.895;
+		System.out.println(number2);
+		boolean bool = true;
+		System.out.println(bool);
+		
+		Integer number4 = 34;
+		System.out.print(number4);
+		Float number3 = 5.34f; 
+		System.out.print(number3);
+		Character word = 'a';
+		System.out.println(word);
+		
+		int[] array = {1,2,3,4,5};
+		System.out.println(array[3]);
+		
+	}
+
+}
