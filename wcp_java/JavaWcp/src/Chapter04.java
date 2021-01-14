288c408cfc0cdc038ae9d19677306a7ecaabfc74
takanari <taka0824nari@gmail.com>
2021-01-14 13:04:19 +0900

    [finish]chapter4

---
+++ wcp_java/JavaWcp/src/Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "taka";
+		System.out.println(name);
+		name = "nishi";
+		System.out.println(name);
+		final int luckyNumber = 7;
+		System.out.println(luckyNumber);
+	}
+
+}
