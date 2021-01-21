d20c7886e289489ec75350e6457cded91c77da2a
西尾貴成 <taka0824nari@gmail.com>
2021-01-21 22:19:09 +0900

    chap1_9_1

---
+++ furigana_php/chap1_7_2.php
@@ -0,0 +1,9 @@
+<?php
+$value = 100;
+$sale = $value * 0.7;
+var_dump($sale);
+
+$value = 150;
+$sale = $value * 0.7;
+var_dump($sale);
+?>
---
+++ furigana_php/chap1_9_1.php
@@ -0,0 +1,5 @@
+<?php
+var_dump("あなたの名前はなんですか");
+$msg = fgets(STDIN);
+echo($msg."さん、こんにちは");
+?>
